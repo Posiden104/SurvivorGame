2aa4898a915ed7aaf1499276aa8bd7ce57b95a6c
Joel Van Auken <posiden104@gmail.com>
2023-03-13 11:01:22 -0400

    Enemy damage timer

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,44 +1,75 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : Entity
 {
     [SerializeField]
     private float damage;
+    [SerializeField] 
+    private float damageFrequencyInSec;
+    [SerializeField] 
+    private float onColExitDamageDelayInSec;
+
     private Rigidbody2D rb;
     private EnemyMovement em;
+    private bool canDamage = true;
+    private bool collidingWithPlayer = false;
+    private int timer = 0;
+    private int fixedUpdatesPerSec = 50;
+    private Player player;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("enemy start");
     }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         em = GetComponent<EnemyMovement>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(collidingWithPlayer && canDamage)
+        {
+            player.Damage(damage);
+            canDamage = false;
+        }
+    }
+
+    // 50x/sec
+    void FixedUpdate()
+    {
+        if (!canDamage)
+        {
+            timer++;
+            if(timer >= damageFrequencyInSec * fixedUpdatesPerSec)
+            {
+                canDamage = true;
+                timer = 0;
+            }
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        var player = other.gameObject.GetComponent<Player>();
-        if (player != null)
+        if (other.gameObject.TryGetComponent<Player>(out var player))
         {
-            player.Damage(damage);
+            collidingWithPlayer = true;
         }
     }
 
-    //private void OnCollisionExit2D(Collision2D other)
-    //{
-    //    Debug.Log("Exit collision");
-    //    em.canMove = true;
-    //}
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        timer = fixedUpdatesPerSec - (int)(onColExitDamageDelayInSec * fixedUpdatesPerSec);
+        collidingWithPlayer = false;
+        Debug.Log(timer);
+    }
 }
