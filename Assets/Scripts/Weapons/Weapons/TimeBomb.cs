27f38867754863de9c345757e49ff726c5a0e13b
Joel Van Auken <posiden104@gmail.com>
2023-09-09 16:55:41 -0400

    fixed time bomb visuals

--- Assets/Scripts/Weapons/Weapons/TimeBomb.cs
+++ Assets/Scripts/Weapons/Weapons/TimeBomb.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts.Weapons
 {
@@ -10,6 +11,11 @@ namespace Assets.Scripts.Weapons
         // Level Up Items
         private float radius = 1f;
         private float delay = 5f;
+
+        private float delayScale = 1.2f;
+        private float delayMin = 0.1f;
+        private float radiusScale = 1.2f;
+        private float radiusMax = 10f;
 
         public TimeBomb(Transform projSpawn) : base(projSpawn)
         {
@@ -27,6 +33,28 @@ namespace Assets.Scripts.Weapons
             bombScript.Setup(delay, radius, damage);
 
             onCooldown = true;
+        }
+
+        public override void LevelUp()
+        {
+            base.LevelUp();
+            if (weaponLevel == 1) return;
+            delay = Mathf.Max(delay / delayScale, delayMin);
+            radius = Mathf.Min(radius * radiusScale, radiusMax);
+        }
+
+        public override string GetLevelUpStats()
+        {
+            var baseStr = base.GetLevelUpStats();
+            var sb = new StringBuilder();
+
+            sb.Append(baseStr);
+            sb.Append($"Fuse Time: {delay}");
+            sb.AppendLine(weaponLevel == 0 ? "" : $" => {Mathf.Max(delay / delayScale, delayMin):n2}");
+            sb.Append($"Blast Radius: {radius}");
+            sb.AppendLine(weaponLevel == 0 ? "" : $" => {Mathf.Min(radius * radiusScale, radiusMax):n2}");
+
+            return sb.ToString();
         }
 
 
